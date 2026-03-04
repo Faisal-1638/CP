d4cad6fdc06a4836461e20b5d7889d9e94f1ff26
Faisal-1638 <fahmed8573@gmail.com>
2026-03-04 19:40:34 +0600

    updated histogram code

---
+++ Loop/histrogram.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ char s;
+ cin>>s;
+ int n;
+ cin>>n;
+
+ for(int i=0; i<n; i++)
+ {
+  int x;
+  cin>>x;
+
+  for(int i=0; i<x; i++)
+  {
+    cout<<s;
+  }
+  cout<<"\n";
+
+ }
+
+return 0;
+
+}

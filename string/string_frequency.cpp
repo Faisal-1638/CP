78cb1fcf74db2551941bef4a3c948cdec7594cf6
Faisal-1638 <fahmed8573@gmail.com>
2026-03-11 00:08:56 +0600

    string frequency added

---
+++ string/string_frequency.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ string s;
+ cin>>s;
+ 
+ int freq[26]= { };
+
+    for(char c : s)
+    {
+        freq[c-'a']++;
+    }
+
+      for(int i=0; i<26; i++)
+      {
+         cout<<char(i+'a')<<": "<<freq[i]<<endl;
+      }
+ 
+return 0;
+ 
+}
